ceb56e4e269b014a661d6c794d173825b947ca9e
Gontran Bezerra <gontranbezerra@gmail.com>
2021-04-10 13:39:22 -0300

    feat: Pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
+import Pagination from '../../components/Pagination';
 
 export default function UsersList() {
   return (
@@ -82,6 +83,7 @@ export default function UsersList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
