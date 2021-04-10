db505d0de34e2ceded6f68a30b89542f129ae519
Gontran Bezerra <gontranbezerra@gmail.com>
2021-04-10 20:28:13 -0300

    feat: Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export default function Profile(props) {
+  const { showProfileData = true } = props;
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gontran Bezerra</Text>
-        <Text color="gray.300" fontSize="small">
-          gontranbezerra@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gontran Bezerra</Text>
+          <Text color="gray.300" fontSize="small">
+            gontranbezerra@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Gontran Bezerra"
